e0a973d176fe35b4501d04f26a3a27b1005cf966
Divya Devanathan <Divya.d@ZIFOCORP.COM>
2024-10-17 18:54:01 +0530

    Ready to merge with main

--- sdttims/urls.py
+++ sdttims/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 from Inventory.views import HomePage
 
 urlpatterns = [
-    path('', HomePage, name='home'),
+    path('', HomePage, name='index'),
     path('admin/', admin.site.urls),
     path('Inventory/', include('Inventory.urls')),   
     path('Booking/', include('Booking.urls')),
