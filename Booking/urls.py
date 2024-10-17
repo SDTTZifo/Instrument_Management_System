8f288adb792167c65ba3cb50ef63e0cbdc98a654
Riyas <sdttzifo@gmail.com>
2024-10-18 00:32:32 +0530

    booking files added

--- Booking/urls.py
+++ Booking/urls.py
@@ -2,8 +2,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from Booking.views import booking_view
+from Booking.views import booking_view, new_booking
 
 urlpatterns = [
-    path('booking/', booking_view, name='booking'),  # Add booking view
+    path('booking/', booking_view, name='booking'),
+    path('new-booking/', new_booking, name='new_booking'),
 ]
--- Booking/views.py
+++ Booking/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def booking_view(request):
-    return render(request, 'booking/booking.html')  # Create booking.html template
+    return render(request, 'booking/booking.html')  
+
+def new_booking(request):
+    return render(request, 'booking/new_booking.html')  
