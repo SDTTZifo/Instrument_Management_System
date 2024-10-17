8d1f8bb4c917a848a0adaaf7ff91cbe916fa7e13
Divya Devanathan <Divya.d@ZIFOCORP.COM>
2024-10-17 17:15:51 +0530

    Removed pycache files from git using gitignore

--- sdttims/urls.py
+++ sdttims/urls.py
@@ -2,7 +2,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from Inventory.views import HomePage
+
 urlpatterns = [
+    path('', HomePage, name='home'),
     path('admin/', admin.site.urls),
     path('Inventory/', include('Inventory.urls')),   
     path('Booking/', include('Booking.urls')),
