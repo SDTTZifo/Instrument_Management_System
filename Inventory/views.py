617d77e679669a61b23dcca2515695f993d4ed0d
Divya Devanathan <Divya.d@ZIFOCORP.COM>
2024-10-18 18:15:34 +0530

    Added serach functionality

--- Inventory/views.py
+++ Inventory/views.py
@@ -16,14 +16,26 @@ def add_instrument_view(request):
     categories = Category.objects.all()
     if request.method == 'POST':
         instrument_name = request.POST.get('instrument_name')
-        instrument_description = request.POST.get('instrument_description')
+        category = request.POST.get('category')
+        model_number = request.POST.get('model_number')
+        manufacturer = request.POST.get('manufacturer')
+        purchased_On = request.POST.get('purchased_On')
         owned_by = request.POST.get('owned_by')
+        location = request.POST.get('location')
+        status = request.POST.get('status')
+        maintenance_date = request.POST.get('maintenance_date')
 
         # Create a new instrument object and save it to the database
         new_instrument = Instrument(
             instrument_name=instrument_name,
-            instrument_description=instrument_description,
-            owned_by=owned_by
+            category= category,
+            model_number = model_number,
+            manufacturer = manufacturer,
+            purchased_On = purchased_On,
+            owned_by =  owned_by,
+            location =  location,
+            status = status,
+            maintenance_date =  maintenance_date    
         )
         new_instrument.save()  # Save to database
 
@@ -58,11 +70,11 @@ def search_instrument_view(request):
     instruments = Instrument.objects.all()
     if query:
         instruments = instruments.filter(
-                                            Q(name__icontains=query)|
+                                            Q(instrument_name__icontains=query)|
                                             Q(category__icontains=query)|
-                                            Q(serial_number__icontains=query)|
+                                            Q(model_number__icontains=query)|
                                             Q(status__icontains=query))
         
                                         
 
-    return render(request, 'instruments/add_instrument.html', {'instruments': instruments, 'query' : query})
+    return render(request, 'instruments/instruments.html', {'instruments': instruments, 'query' : query})
